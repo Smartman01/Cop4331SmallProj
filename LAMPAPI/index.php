c33ff56d02ee8e8fe660e033f4d033109791407b
jordanmcm <jmcmillan@knights.ucf.edu>
2021-01-16 13:16:15 -0500

    Add .gitignore and LAMPAPI folder

---
+++ LAMPAPI/index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+</head>
+<body>
+    <p>probably going to put some redirect here</p>
+    <p>this page currently just exists to disable autoindexing for this directory without modifying apache configuration</p>
+</body>
+</html>
