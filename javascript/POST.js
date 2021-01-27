f98666c6c4fe4a6557e3af256e9982972d6e0acc
Carl Antoine <carlantoine14@gmail.com>
2021-01-27 18:57:41 -0500

    push

--- javascript/POST.js
+++ javascript/POST.js
@@ -9,45 +9,75 @@ const addContactAPI = "/AddContact.php"
 const modContactAPI = "/ModifyContact.php"
 const removeContactAPI = "/RemoveContact.php"
 
-function login() {
+function login() 
+{
     let username = document.getElementById("userlogin").value;
     let password = document.getElementById("userpassword").value;
 
-    var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
+    // var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
 
-    var xhr = new XMLHttpRequest();
-	xhr.open("POST", baseUrl + loginAPI, false);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try
-    {
-        xhr.send(jsonPayload);
+    // var xhr = new XMLHttpRequest();
+	// xhr.open("POST", baseUrl + loginAPI, false);
+    // xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    // try
+    // {
+    //     xhr.send(jsonPayload);
 		
-        var jsonObject = JSON.parse( xhr.responseText );
+    //     var jsonObject = JSON.parse( xhr.responseText );
         
-        alert(jsonObject);
-    }
-    catch (err)
+    //     alert(jsonObject);
+    // }
+    // catch (err)
+    // {
+    //     alert(err);
+    // }
+    
+    
+    fetch(baseUrl + loginAPI,
     {
-        alert(err);
-    }
-    
-    
-    // fetch(baseUrl + loginAPI,
-    // {
-    //     method: "POST",
-    //     headers:
-    //     {
-    //         "Content-type" : "application/json; charset=UTF-8"
-    //     },
-    //     body:
-    //     {
-    //         username: username,
-    //         password: password
-    //     }
-    // })
-    // .then(res =>
-    // {
-    //     alert(res);
-    // })
-    // .catch(res => alert(res));
+        method: "POST",
+        headers:
+        {
+            "Content-type" : "application/json; charset=UTF-8"
+        },
+        body:
+        {
+            username: username,
+            password: password
+        }
+    })
+    .then(res =>
+    {
+        alert(res);
+    })
+    .catch(res => alert(res));
+}
+
+function register()
+{
+    let first = document.getElementById("first_name").value;
+    let last = document.getElementById("last_name").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    fetch(baseUrl + createAPI,
+    {
+        method: "POST",
+        headers:
+        {
+            "Content-type" : "application/json; charset=UTF-8"
+        },
+        body:
+        {
+            firstName: first,
+            lastName: last,
+            username: username,
+            password: password
+        }
+    })
+    .then(res =>
+    {
+        alert(res);
+    })
+    .catch(res => alert(res));
 }
