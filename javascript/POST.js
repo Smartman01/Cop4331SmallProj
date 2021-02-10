f229b8eda27e9092942185d2d3deb01d9fb64c7d
Christian Santiago <c.santiago16.cs@knights.ucf.edu>
2021-02-10 12:27:18 -0500

    Revert "function update and searchResult p tag"
    This reverts commit bd5efb735fbc0bfa99c2118ee9b215031488ee41.

--- javascript/POST.js
+++ javascript/POST.js
@@ -128,7 +128,7 @@ function edit()
     // }
 }
 
-function deleteContact(id)
+function deleteContact()
 {
     // let id = document.getElementById("selectedID").value;
 
@@ -177,7 +177,7 @@ function searchContact()
 	// 			for (var i = 0; i< jsonObject.contacts.length; i++)
 	// 			{
     //                 let contact = `Name: ${jsonObject.contacts[i].firstName} ${jsonObject.contacts[i].lastName} Phone: ${jsonObject.contacts[i].phone} Email: ${jsonObject.contacts[i].email} ID: ${jsonObject.contacts[i].id}`
-	// 				list += `<p id="${jsonObject.contacts[i].id}">${contact} <button type="submit" onclick="${deleteContact(jsonObject.contacts[i].id)}">Delete Contact</button></p>`;
+	// 				list += `<p id="${jsonObject.contacts[i].id}">${contact}</p>`;
 
 	// 				if (i < jsonObject.results.length - 1)
 	// 				{
@@ -185,7 +185,7 @@ function searchContact()
 	// 				}
 	// 			}
 				
-	// 			document.getElementById("searchResults").innerHTML = list;
+	// 			document.getElementsByTagName("p")[0].innerHTML = list;
 	// 		}
 	// 	};
 	// 	xhr.send(jsonPayload);
